6d98d44f10297e2b276a813ed64745c5ec681248
Alex Smith <alexsmith716@outlook.com>
2017-12-25 23:24:50 -0500

    More client additions for api request handling.

---
+++ client/services/apiHandler.js
@@ -0,0 +1,14 @@
+
+import fetch from 'isomorphic-fetch';
+import promises from 'es6-promise';
+// or maybe use some other HTTP request module for requests;
+
+promises.polyfill();
+
+
+
+export default function apiHandler () {
+
+  // will be handling api requests from client 
+
+};
