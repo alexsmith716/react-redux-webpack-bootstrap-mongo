878d47a56ab450e3ab51cf2515c98fba239c7df0
Alex Smith <alexsmith716@outlook.com>
2018-01-15 22:35:05 -0500

    Decided to use react router v3, not v4. There is more extensive and proven coverage with v3 as with protected routes.

--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -10,6 +10,7 @@ import { NavLink } from 'react-router-dom';
 //  </button>
 // <li className="nav-item"><NavLink className="nav-link" to="#">Sign Up</NavLink></li>
 // <li className="nav-item"><NavLink className="nav-link" to="#">Log In</NavLink></li>
+//     <nav className="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse" role="navigation">
 
 export function Header(props, context) {
 
@@ -17,31 +18,51 @@ export function Header(props, context) {
 
   return (
 
-    <nav className="navbar navbar-toggleable-md navbar-custom fixed-top" role="navigation">
-      <div className="container">
-        <div className="navbar-header"><NavLink className="navbar-brand nav-link" to="/">Election App 2018</NavLink>
-          <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target=".navbar-main-collapse"><img src="/public/static/images/icon-bar-36.svg" alt="menu icon"></img></button>
-        </div>
-        <div className="navbar-collapse navbar-main-collapse collapse">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item active"><NavLink className="nav-link" to="/">Home</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/about">About</NavLink></li>
-            <li className="nav-item"><NavLink className="nav-link" to="/contact">Contact</NavLink></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  <nav className="container w-100">
+    <div className="d-flex justify-content-between fixed-top hidden-lg-up">
+      <NavLink className="navbar-brand nav-link" to="">Bootstrap</NavLink>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#bd-main-nav" aria-controls="bd-main-nav" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+    </div>
+
+    <div className="collapse navbar-collapse" id="bd-main-nav">
+
+      <ul className="nav navbar-nav">
+
+        <li className="nav-item active">
+          <NavLink className="nav-link " to="">Bootstrap</NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink className="nav-link active" to="">Documentation</NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink className="nav-link " to="">Examples 1</NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink className="nav-link " to="">Examples 2</NavLink>
+        </li>
+
+        <li className="nav-item">
+          <NavLink className="nav-link " to="">Examples 3</NavLink>
+        </li>
+
+      </ul>
+
+    </div>
+
+  </div>
 
   );
 }
 
 Header.contextTypes = {
-  router: PropTypes.object,
 };
 
 Header.propTypes = {
-  isLoggedIn: PropTypes.bool.isRequired,
-  registered: PropTypes.bool.isRequired,
 };
 
 export default Header;
